fc17320029602c8c8fbc382c62ca1aa1627a317f
RadityaNugra <mraditnp@gmail.com>
2021-10-12 13:17:10 +0700

    aplod template

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::get('/', function () {
 });
 
 
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
-});
+// Route::group(['prefix' => 'admin'], function () {
+//     Voyager::routes();
+// });
