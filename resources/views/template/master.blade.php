5e2c1c386c5bab77ebd9ed84ea0e2e91d789380c
waladahlmzaqi <walada204@gmail.com>
2021-10-12 17:48:01 +0700

    update

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -11,15 +11,15 @@
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 
 <!-- Template CSS -->
-<link rel="stylesheet" href="{{asset('template/assets/css/style.css')}}">
-<link rel="stylesheet" href="{{asset('template/assets/css/components.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/components.css')}}">
 
 <!-- CSS Native -->
 @stack('link')
 
 </head>
 
-<body>
+<body class="layout-2">
 <div id="app">
     <div class="main-wrapper main-wrapper-1">
     <div class="navbar-bg"></div>
@@ -27,6 +27,10 @@
     {{-- navbar  --}}
     @include('template.master.navbar')
     {{-- end navbar --}}
+
+    {{-- sidebar --}}
+    @include('template.master.sidebar')
+    {{-- endsidebar --}}
 
 
 
@@ -60,14 +64,14 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-<script src="{{ asset('template/assets/js/stisla.js') }}"></script>
+<script src="{{ asset('assets/js/stisla.js') }}"></script>
 
 <!-- JS Libraries -->
 @stack('script')
 
 <!-- Template JS File -->
-<script src="{{ asset('template/assets/js/scripts.js') }}"></script>
-<script src="{{ asset('template/assets/js/custom.js') }}"></script>
+<script src="{{ asset('assets/js/scripts.js') }}"></script>
+<script src="{{ asset('assets/js/custom.js') }}"></script>
 
 </body>
 </html>
