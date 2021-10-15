dd41a09ac5da580b25fe369c153406b58763b12d
RadityaNugra <mraditnp@gmail.com>
2021-10-15 08:54:07 +0700

    dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,24 +8,69 @@
 @endsection
 @section('main')
     <div class="section-body">
-        <h2 class="section-title">This is Example Page</h2>
-        <p class="section-lead">This page is just an example for you to create your own page.</p>
-        <div class="card">
-            <div class="card-header">
-                <h4>Example Card</h4>
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <div class="card card-statistic-2">
+                  <div class="card-stats">
+                    <div class="card-stats-items mt-4">
+                      <div class="card-stats-item">
+                        <div class="card-stats-item-count">24</div>
+                        <div class="card-stats-item-label">Pending</div>
+                      </div>
+                      <div class="card-stats-item">
+                        <div class="card-stats-item-count">12</div>
+                        <div class="card-stats-item-label">Shipping</div>
+                      </div>
+                      <div class="card-stats-item">
+                        <div class="card-stats-item-count">23</div>
+                        <div class="card-stats-item-label">Completed</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="card-icon shadow-primary bg-primary">
+                    <i class="fas fa-archive"></i>
+                  </div>
+                  <div class="card-wrap">
+                    <div class="card-header">
+                      <h4>Total Orders</h4>
+                    </div>
+                    <div class="card-body">
+                      59
+                    </div>
+                  </div>
+                </div>
+              </div>
+            <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+              <div class="card card-statistic-1">
+                <div class="card-icon bg-danger">
+                  <i class="far fa-newspaper"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Total</h4>
+                  </div>
+                  <div class="card-body">
+                    42
+                  </div>
+                </div>
+              </div>
             </div>
-            <div class="card-body">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-            <div class="card-footer bg-whitesmoke">
-                This is card footer
-            </div>
-        </div>
+            <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+              <div class="card card-statistic-1">
+                <div class="card-icon bg-warning">
+                  <i class="far fa-file"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Total</h4>
+                  </div>
+                  <div class="card-body">
+                    1,201
+                  </div>
+                </div>
+              </div>
+            </div>                 
+          </div>
     </div>
 @endsection
 
